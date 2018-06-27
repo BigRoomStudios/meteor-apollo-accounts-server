8793c0b6ee8779b49d830825b6009b4ccde07299
WilliamSWoodruff <williamswoodruff@gmail.com>
2018-06-27 14:59:01 -0400

    Init copy over from bigroomstudios/meteor-apollo-accounts

---
+++ src/Mutation/index.js
@@ -0,0 +1,31 @@
+import loginWithPassword from './loginWithPassword'
+import loginWithToken from './loginWithToken'
+import logout from './logout'
+import changePassword from './changePassword'
+import createUser from './createUser'
+import verifyEmail from './verifyEmail'
+import resendVerificationEmail from './resendVerificationEmail'
+import forgotPassword from './forgotPassword'
+import resetPassword from './resetPassword'
+import oauth from './oauth'
+import hasService from './oauth/hasService'
+
+export default function (options) {
+  const resolvers = {
+    logout,
+    verifyEmail,
+    resendVerificationEmail,
+    ...oauth(options)
+  }
+
+if (hasService('password')) {
+  resolvers.loginWithPassword = loginWithPassword
+  resolvers.loginWithToken = loginWithToken
+  resolvers.changePassword = changePassword
+  resolvers.createUser = createUser
+  resolvers.forgotPassword = forgotPassword
+  resolvers.resetPassword = resetPassword
+}
+
+  return {Mutation: resolvers}
+}
